656012e62b5e65ca24fe3a9b7f7cb597adab3e7e
santos859 <miguelrangel245@icloud.com>
2026-03-13 18:43:44 -0300

    texto

---
+++ 02032026.py
@@ -0,0 +1,89 @@
+# print('olá mundo!')
+
+# pokebola1a1='Bulbasaur'
+# pokebo1a2='Psyduck'
+# pokebp1a3='Clefairy'
+# idade=30
+# nome='Maria'
+# preco=19.99
+
+# data1='04-03-2026'
+# vazio=None
+# print(vazio)
+# decisao=True
+# decisao1=False
+# print(type(decisao))
+
+
+
+
+# nota1=4
+# nota2=6
+# media =(+nota2)/2
+# soma=+nota2
+# subtração=-nota2
+# divisao=/nota2
+# multiplicacao=*nota2
+# print(media)
+# print(soma)
+# print(subtração)
+# print(divisao)
+# print(multiplicacao)
+
+
+# num1=3
+# num2=4
+
+# soma=num1+um2
+# subtracao=num1-num2
+# multiplicacao=num1+num2
+# divisao=num1/num2
+# modulo=num1%num2
+# print(soma,subtracao,multiplicacao,divisao,modulo)
+
+# # if placar_time1 > placar_time2:#IDENTAÇÂO
+    
+# primeiro_nome ='Maria'
+# sobrenome = 'Silva'
+# nome_completo = primeiro_nome + " " + sobrenome
+# print(nome_completo)
+# print(f"o tipo armazenado /n"
+#        f"na variável 'primeiro_ nome' é{type(primeiro_nome)}")
+
+
+# tem_carteira = True
+# idade = 18
+# tem_carro = False
+# bebe=True
+# pode_dirigir = (idade >=18 and tem_carteira) and bebe==False
+# print ("pode dirigir?", pode_dirigir)
+# print("pode dirigir e tem carro?",pode_dirigir or tem_carro)
+    
+
+# frase = "Python é divertido"
+# print(frase.upper())
+# nova_frase = frase.replace("divertido","poderoso")
+# print(nova_frase)
+
+
+# contador =0
+# contador +=5 # contador=contador+5
+# contador -=2 #contador=contador -2
+# contador *=3 #contador=contador*3
+# print("valor final do contador:" ,contador)
+
+
+# itens_carrinho=0
+# itens_carrinho=itens_carrinho+1
+# print(itens_carrinho)
+
+
+a=10
+b=20
+c=30
+media = (a+b+c)/3
+print("media:" , media)
+print("a media é maior que 15 menor que 25?",15<media<25)
+
+
+
---
+++ teste.py
@@ -0,0 +1,6 @@
+a=10
+b=20
+c=30
+media = (a+b+c)/3
+print("media:" , media)
+print("a media é maior que 15 menor que 25?",15<media<25)
