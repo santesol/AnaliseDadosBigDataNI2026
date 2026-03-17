5cc72e6669cecddae3b3d05af57fbcc91ed1a5ff
santos859 <miguelrangel245@icloud.com>
2026-03-16 21:59:58 -0300

    adicione aula6.py

---
+++ aula6.py
@@ -0,0 +1,29 @@
+# Listas:
+#indices   0         1      2         3      4 ou -1
+# nomes=['Matheus','Alice','Caio', 'Larissa','Miguel','Rafael']
+
+# nomes1=[0]
+
+# print(nomes[-1])
+# print(nomes[-2])
+# print(nomes[-3])
+# print(nomes[-4])
+# print(nomes[-5])
+
+# primeira_parte=nomes[0:3]
+# print(primeira_parte)
+# segunda_parte=nomes[3:6]
+# print(segunda_parte)
+
+# print(len(nomes))
+
+
+# nomes_set=set(nomes)
+# print(nomes_set)
+# #DICIONÁRIOS
+# nomees_dict={1:'Matheus',
+#              2:'Alice',
+#              3:'Larrissa',
+#              4:'Caio',
+#              5:'Miguel',
+#              6:'Rafael'}
